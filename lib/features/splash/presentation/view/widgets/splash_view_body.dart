f5435c884beabb1ae330cd782f2a023736bd615c
mostafa <Moustafa_20225056@fci.helwan.edu.eg>
2025-07-05 01:19:00 +0300

    splash view UI Done

---
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:eqraaly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(
+          AssetsData.logo,
+        ),
+      ],
+    );
+  }
+}
