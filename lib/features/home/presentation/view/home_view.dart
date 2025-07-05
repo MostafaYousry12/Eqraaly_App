4082db1c8db40a7064e07d17d7ddc8025c9c2bce
mostafa <Moustafa_20225056@fci.helwan.edu.eg>
2025-07-05 22:58:41 +0300

    add  listview builder to home view

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,4 +1,6 @@
-import 'package:eqraaly_app/features/home/presentation/view/widgets/app_bar.dart';
+import 'package:eqraaly_app/core/utils/assets.dart';
+import 'package:eqraaly_app/features/home/presentation/view/widgets/custom_app_bar.dart';
+import 'package:eqraaly_app/features/home/presentation/view/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -9,8 +11,29 @@ class HomeView extends StatelessWidget {
     return const Scaffold(
       body: Column(
         children: [
-          AppBarWidget(),
+          CustomAppBar(),
+          CustomListView(),
         ],
+      ),
+    );
+  }
+}
+
+class CustomListView extends StatelessWidget {
+  const CustomListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 6),
+            child: CustomListViewItem(),
+          );
+        },
       ),
     );
   }
--- lib/features/home/presentation/view/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/view/widgets/custom_app_bar.dart
@@ -2,13 +2,13 @@ import 'package:eqraaly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class AppBarWidget extends StatelessWidget {
-  const AppBarWidget({super.key});
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+      padding: const EdgeInsets.only(left: 16, top: 40, right: 16, bottom: 10),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
