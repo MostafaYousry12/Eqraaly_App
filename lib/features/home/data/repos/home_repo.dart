11ffe89d89526677177acf3d5abf9432b0c99f13
mostafa <Moustafa_20225056@fci.helwan.edu.eg>
2025-07-07 02:10:03 +0300

    added Error folder and failures

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:eqraaly_app/core/errors/failure.dart';
+import 'package:eqraaly_app/features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
