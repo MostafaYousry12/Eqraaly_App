8938271596bd551d605295f191160b061aa90f2e
mostafa <Moustafa_20225056@fci.helwan.edu.eg>
2025-07-07 05:43:20 +0300

    create server failure from dio error

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:eqraaly_app/core/errors/failure.dart';
 import 'package:eqraaly_app/core/utils/api_services.dart';
 import 'package:eqraaly_app/features/home/data/models/book_model/book_model.dart';
@@ -22,7 +23,11 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
     }
   }
 
